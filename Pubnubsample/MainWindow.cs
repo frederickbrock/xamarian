03903d0a8c6f128f3dfee27af0dadd3bb5c3ed3e
Frederick Brock <frederick.brock@gmail.com>
2015-10-29 17:39:29 -0700

    initial commit

---
+++ Pubnubsample/MainWindow.cs
@@ -0,0 +1,70 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using MonoMac.Foundation;
+using MonoMac.AppKit;
+
+using PubNubMessaging.Core;
+
+namespace Pubnubsample
+{
+	public partial class MainWindow : MonoMac.AppKit.NSWindow
+	{
+		#region Constructors
+		private Pubnub _pubnub;
+
+		// Called when created from unmanaged code
+		public MainWindow (IntPtr handle) : base (handle)
+		{
+			Initialize ();
+		}
+		
+		// Called when created directly from a XIB file
+		[Export ("initWithCoder:")]
+		public MainWindow (NSCoder coder) : base (coder)
+		{
+			Initialize ();
+		}
+		
+		// Shared initialization code
+		void Initialize ()
+		{
+			GetPubnub ().Subscribe<string> ("MSFT", MessageReceived, ConnectCallback, ErrorCallback);
+			GetPubnub ().Subscribe<string> ("AMZN", MessageReceived, ConnectCallback, ErrorCallback);
+			GetPubnub ().Subscribe<string> ("MSFT", MessageReceived, ConnectCallback, ErrorCallback);
+		}
+
+
+		public Pubnub GetPubnub()
+		{
+			if (this._pubnub == null) {
+				this._pubnub = new Pubnub ("demo", "demo");
+			}
+
+
+			return(_pubnub);
+		}
+
+		public void MessageReceived(String message)
+		{
+			Console.WriteLine ("Message received");
+			
+		}
+
+
+		public void ConnectCallback(string message)
+		{
+		}
+
+		public void ErrorCallback(PubnubClientError message)
+		{
+		}
+
+
+
+
+		#endregion
+	}
+}
+
---
+++ Pubnubsample/MainWindowController.cs
@@ -0,0 +1,52 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using MonoMac.Foundation;
+using MonoMac.AppKit;
+
+using PubNubMessaging.Core;
+
+
+namespace Pubnubsample
+{
+	public partial class MainWindowController : MonoMac.AppKit.NSWindowController
+	{
+		private Pubnub _pubnub;
+		#region Constructors
+
+		// Called when created from unmanaged code
+		public MainWindowController (IntPtr handle) : base (handle)
+		{
+			Initialize ();
+		}
+		
+		// Called when created directly from a XIB file
+		[Export ("initWithCoder:")]
+		public MainWindowController (NSCoder coder) : base (coder)
+		{
+			Initialize ();
+		}
+		
+		// Call to load from the XIB/NIB file
+		public MainWindowController () : base ("MainWindow")
+		{
+			Initialize ();
+		}
+		
+		// Shared initialization code
+		void Initialize ()
+		{
+		}
+
+		#endregion
+
+		//strongly typed window accessor
+		public new MainWindow Window {
+			get {
+				return (MainWindow)base.Window;
+			}
+		}
+	}
+}
+
